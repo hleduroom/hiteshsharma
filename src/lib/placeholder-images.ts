d25b7ce2b66acff784493e0d4e695b23f77e1c3c
H.L.-Eduroom <hiteshsharma2080@icloud.com>
2025-10-20 21:10:10 +0545

    Update placeholder-images.ts

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -2,9 +2,12 @@ import data from './placeholder-images.json';
 
 export type ImagePlaceholder = {
   id: string;
+  name: string;      // Added for CardTitle and Schema.org
   description: string;
   imageUrl: string;
   imageHint: string;
+  url: string;       // Added for active project link and Schema.org
+  tags: string[];    // Added for Schema.org keywords
 };
 
-export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages;
+export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages as ImagePlaceholder[];
