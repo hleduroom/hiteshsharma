070cc5ad0a31cc87727a97e99f9dc3b9db863120
H.L.-Eduroom <hiteshsharma2080@icloud.com>
2025-10-21 19:19:46 +0545

    Update page.tsx

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -10,7 +10,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-import Header from '@/components/header';
+import { Header } from "@/components/header";
 import { useCart } from '@/lib/context/CartContext';
 
 interface BookDetailsPageProps {
