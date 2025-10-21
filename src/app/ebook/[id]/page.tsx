1fd944402e49fbe42e9159a1779c558b84dd7906
H.L.-Eduroom <hiteshsharma2080@icloud.com>
2025-10-21 10:45:00 +0545

    Update page.tsx

--- src/app/ebook/[id]/page.tsx
+++ src/app/ebook/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from 'react';
-import { bookData } from '@/lib/data/book';
+import { mainBook, allBooks } from '@/lib/data/books';
 import { notFound, useParams } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
