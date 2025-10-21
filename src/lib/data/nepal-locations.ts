17abed33dfe98f55423718851ee50eab5da25ae6
H.L.-Eduroom <hiteshsharma2080@icloud.com>
2025-10-21 22:46:18 +0545

    Create nepal-locations.ts

---
+++ src/lib/data/nepal-locations.ts
@@ -0,0 +1,39 @@
+export const NEPAL_LOCATIONS = {
+  provinces: [
+    {
+      id: 1,
+      name: 'Province 1',
+      districts: ['Jhapa', 'Morang', 'Sunsari', 'Ilam', 'Dhankuta', 'Taplejung', 'Panchthar']
+    },
+    {
+      id: 2,
+      name: 'Madhesh Province',
+      districts: ['Sarlahi', 'Mahottari', 'Dhanusha', 'Siraha', 'Saptari', 'Parsa', 'Bara', 'Rautahat']
+    },
+    {
+      id: 3,
+      name: 'Bagmati Province',
+      districts: ['Kathmandu', 'Lalitpur', 'Bhaktapur', 'Kavrepalanchok', 'Dhading', 'Nuwakot', 'Rasuwa', 'Sindhupalchok', 'Dolakha']
+    },
+    {
+      id: 4,
+      name: 'Gandaki Province',
+      districts: ['Pokhara', 'Kaski', 'Syangja', 'Tanahu', 'Lamjung', 'Gorkha', 'Manang', 'Mustang']
+    },
+    {
+      id: 5,
+      name: 'Lumbini Province',
+      districts: ['Rupandehi', 'Nawalparasi', 'Kapilvastu', 'Palpa', 'Arghakhanchi', 'Gulmi', 'Rolpa', 'Pyuthan']
+    },
+    {
+      id: 6,
+      name: 'Karnali Province',
+      districts: ['Surkhet', 'Dailekh', 'Jajarkot', 'Dolpa', 'Jumla', 'Kalikot', 'Mugu', 'Humla']
+    },
+    {
+      id: 7,
+      name: 'Sudurpashchim Province',
+      districts: ['Kailali', 'Kanchanpur', 'Dadeldhura', 'Baitadi', 'Darchula', 'Bajhang', 'Bajura', 'Doti', 'Achham']
+    }
+  ]
+};
