edbd6c6982c32a04080effcd16cdd44fcd77347c
H.L.-Eduroom <hiteshsharma2080@icloud.com>
2025-12-20 23:21:42 +0545

    Update nepal-locations.ts

--- src/lib/data/nepal-locations.ts
+++ src/lib/data/nepal-locations.ts
@@ -3,37 +3,37 @@ export const NEPAL_LOCATIONS = {
     {
       id: 1,
       name: 'Province 1',
-      districts: ['Jhapa', 'Morang', 'Sunsari', 'Ilam', 'Dhankuta', 'Taplejung', 'Panchthar']
+      districts: ['Bhojpur', 'Dhankuta', 'Ilam', 'Jhapa', 'Khotang', 'Morang', 'Okhaldhunga', 'Panchthar', 'Sankhuwasabha', 'Solukhumbu', 'Sunsari', 'Taplejung', 'Terhathum', 'Udayapur']
     },
     {
       id: 2,
-      name: 'Madhesh Province',
-      districts: ['Sarlahi', 'Mahottari', 'Dhanusha', 'Siraha', 'Saptari', 'Parsa', 'Bara', 'Rautahat']
+      name: 'Province 2',
+      districts: ['Parsa', 'Bara', 'Rautahat', 'Sarlahi', 'Dhanusha', 'Mahottari', 'Siraha', 'Saptari']
     },
     {
       id: 3,
       name: 'Bagmati Province',
-      districts: ['Kathmandu', 'Lalitpur', 'Bhaktapur', 'Kavrepalanchok', 'Dhading', 'Nuwakot', 'Rasuwa', 'Sindhupalchok', 'Dolakha']
+      districts: ['Kathmandu', 'Lalitpur', 'Bhaktapur', 'Kavrepalanchok', 'Dhading', 'Nuwakot', 'Rasuwa', 'Sindhupalchok', 'Dolakha', 'Ramechhap', 'Sindhuli', 'Makwanpur', 'Chitwan']
     },
     {
       id: 4,
       name: 'Gandaki Province',
-      districts: ['Pokhara', 'Kaski', 'Syangja', 'Tanahu', 'Lamjung', 'Gorkha', 'Manang', 'Mustang']
+      districts: ['Baglung', 'Gorkha', 'Kaski', 'Lamjung', 'Manang', 'Mustang', 'Myagdi', 'Nawalpur', 'Parbat', 'Syangja', 'Tanahun']
     },
     {
       id: 5,
       name: 'Lumbini Province',
-      districts: ['Rupandehi', 'Nawalparasi', 'Kapilvastu', 'Palpa', 'Arghakhanchi', 'Gulmi', 'Rolpa', 'Pyuthan']
+      districts: ['Kapilvastu', 'Parasi', 'Rupandehi', 'Arghakhanchi', 'Gulmi', 'Palpa', 'Dang', 'Pyuthan', 'Rolpa', 'Eastern Rukum', 'Banke', 'Bardiya']
     },
     {
       id: 6,
       name: 'Karnali Province',
-      districts: ['Surkhet', 'Dailekh', 'Jajarkot', 'Dolpa', 'Jumla', 'Kalikot', 'Mugu', 'Humla']
+      districts: ['Western Rukum', 'Salyan', 'Dolpa', 'Humla', 'Jumla', 'Kalikot', 'Mugu', 'Surkhet', 'Dailekh', 'Jajarkot']
     },
     {
       id: 7,
       name: 'Sudurpashchim Province',
-      districts: ['Kailali', 'Kanchanpur', 'Dadeldhura', 'Baitadi', 'Darchula', 'Bajhang', 'Bajura', 'Doti', 'Achham']
+      districts: ['Kailali', 'Achham', 'Doti', 'Bajhang', 'Bajura', 'Kanchanpur', 'Dadeldhura', 'Baitadi', 'Darchula']
     }
   ]
 };
